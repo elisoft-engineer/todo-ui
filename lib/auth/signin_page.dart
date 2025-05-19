369cee048c661882018a06949646056f3a6ccc1d
Elkana Maina <elisoft.engineer@gmail.com>
2025-05-19 13:05:26 +0300

    Add the form

--- lib/auth/signin_page.dart
+++ lib/auth/signin_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todo/core/styles.dart';
+import 'package:todo/core/services.dart';
 import 'package:todo/components/app_bar.dart';
 
 class SigninPage extends StatefulWidget {
@@ -10,22 +13,157 @@ class SigninPage extends StatefulWidget {
 }
 
 class _SigninPageState extends State<SigninPage> {
-  Future<void> signin() async {}
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool isLoading = false;
+
+  Future<void> signin() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      final response = await APIService.post('token/', {
+        'email': emailController.text.trim(),
+        'password': passwordController.text.trim(),
+      }, auth: false);
+      setState(() {
+        isLoading = false;
+      });
+      print(response);
+      if (response.containsKey('error')) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(response['error']),
+            backgroundColor: CustomColors.error,
+          ),
+        );
+        return;
+      } else if (response.containsKey('Server')) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Server is having issues"),
+            backgroundColor: CustomColors.error,
+          ),
+        );
+        return;
+      } else {
+        final String accessToken = response['access'];
+        final String refreshToken = response['refresh'];
+        final String id = response['id'];
+        final String email = response['email'];
+        final String firstName = response['first_name'];
+        final String lastName = response['last_name'];
+        bool isActive = response['is_active'];
+        bool isStaff = response['is_staff'];
+
+        final prefs = await SharedPreferences.getInstance();
+        prefs.setString('access_token', accessToken);
+        prefs.setString('refresh_token', refreshToken);
+        prefs.setString('id', id);
+        prefs.setString('email', email);
+        prefs.setString('first_name', firstName);
+        prefs.setString('last_name', lastName);
+        prefs.setBool('is_active', isActive);
+        prefs.setBool('is_staff', isStaff);
+
+        Navigator.of(context).pushReplacementNamed("todo");
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Signed in as $email"),
+            backgroundColor: CustomColors.success,
+          ),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Server is not responding"),
+          backgroundColor: CustomColors.error,
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TopBar(title: "Sign In"),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              spacing: 20,
+              children: [
+                Text(
+                  "Sign in to continue",
+                  style: CustomTextStyles.h3.copyWith(
+                    color: CustomColors.textColor,
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                  child: TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    style: CustomTextStyles.b3.copyWith(
+                      color: CustomColors.textColor,
+                    ),
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: CustomColors.textColor),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: CustomColors.textColor),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: CustomColors.textColor),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                  child: TextField(
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    style: CustomTextStyles.b3,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: CustomColors.textColor),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: CustomColors.textColor),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: CustomColors.textColor),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 40,
+                  child:
+                      isLoading
+                          ? const CircularProgressIndicator()
+                          : ElevatedButton(
+                            onPressed: signin,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: CustomColors.primaryColor,
+                              foregroundColor: CustomColors.background,
+                            ),
+                            child: Text("Sign In", style: CustomTextStyles.b2),
+                          ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
