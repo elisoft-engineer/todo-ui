811c07b6483573c910385abfa7e5b41a76a911a1
Elkana Maina <elisoft.engineer@gmail.com>
2025-05-23 14:58:04 +0300

    Initial Commit

--- lib/tasks/todo.dart
+++ lib/tasks/todo.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:todo/core/styles.dart';
 
 class TodoPage extends StatelessWidget {
   const TodoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: Text(
+        "Todo",
+        style: CustomTextStyles.h1.copyWith(color: CustomColors.textColor),
+      ),
+    );
   }
 }
