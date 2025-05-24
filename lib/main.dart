f56d8a39de460a0214cbaafc9c5eecb2716bea89
Elkana Maina <elisoft.engineer@gmail.com>
2025-05-24 09:50:59 +0300

    Load the env variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:todo/auth/signin_page.dart';
 import 'package:todo/tasks/main.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
