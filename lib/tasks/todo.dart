f27cb8fa2da49f4ece8c36248a478a34e7a431a1
Elkana Maina <elisoft.engineer@gmail.com>
2025-05-24 09:52:56 +0300

    Fetch the Todo tasks

--- lib/tasks/todo.dart
+++ lib/tasks/todo.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:todo/core/styles.dart';
+import 'package:todo/tasks/models.dart';
+import 'package:todo/tasks/services.dart';
 
-class TodoPage extends StatelessWidget {
+class TodoPage extends StatefulWidget {
   const TodoPage({super.key});
+
+  @override
+  State<TodoPage> createState() => _TodoPageState();
+}
+
+class _TodoPageState extends State<TodoPage> {
+  final Future<List<Task>> tasks = TaskService.fetchTasks("todo");
 
   @override
   Widget build(BuildContext context) {
