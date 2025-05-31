e42955748a6c8a724d6a6f94485edb70d477a2b6
Elkana Maina <elisoft.engineer@gmail.com>
2025-05-31 21:55:09 +0300

    Add actions attribute

--- lib/tasks/services.dart
+++ lib/tasks/services.dart
@@ -2,7 +2,7 @@ import 'package:todo/core/services.dart';
 import 'package:todo/tasks/models.dart';
 
 class TaskService {
-  static Future<List<Task>> fetchTasks(String status) async {
+  static Future<List<Task>> fetchTasks({required String status}) async {
     try {
       final response = await APIService.get(
         'tasks/?status=$status',
@@ -22,6 +22,26 @@ class TaskService {
       throw Exception('Unexpected response format');
     } catch (e) {
       throw Exception('Failed to load tasks: ${e.toString()}');
+    }
+  }
+
+  static Future<void> createTask({
+    required String detail,
+    required int priority,
+  }) async {
+    try {
+      final response = await APIService.post('tasks/', {
+        'detail': detail,
+        'priority': priority,
+      }, auth: true);
+
+      if (response is Map && response.containsKey('error')) {
+        throw Exception(response['error']);
+      }
+
+      return;
+    } catch (e) {
+      throw Exception('Failed to create task: ${e.toString()}');
     }
   }
 }
