860a77ce176fda9d08c6f74d0e5eff025d4f5cdf
Elkana Maina <elisoft.engineer@gmail.com>
2025-06-02 14:45:03 +0300

    Modify font sizes

--- lib/core/styles.dart
+++ lib/core/styles.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class CustomTextStyles {
-  static const h1 = TextStyle(fontSize: 40, fontWeight: FontWeight.w900);
-  static const h2 = TextStyle(fontSize: 32, fontWeight: FontWeight.w700);
-  static const h3 = TextStyle(fontSize: 25, fontWeight: FontWeight.w600);
+  static const h1 = TextStyle(fontSize: 24, fontWeight: FontWeight.w900);
+  static const h2 = TextStyle(fontSize: 20, fontWeight: FontWeight.w700);
+  static const h3 = TextStyle(fontSize: 16, fontWeight: FontWeight.w600);
 
-  static const b1 = TextStyle(fontSize: 18, fontWeight: FontWeight.w700);
+  static const b1 = TextStyle(fontSize: 16, fontWeight: FontWeight.w700);
   static const b2 = TextStyle(fontSize: 16, fontWeight: FontWeight.w600);
-  static const b3 = TextStyle(fontSize: 14, fontWeight: FontWeight.w500);
+  static const b3 = TextStyle(fontSize: 16, fontWeight: FontWeight.w500);
 
   static const s1 = TextStyle(fontSize: 14, fontWeight: FontWeight.w400);
   static const s2 = TextStyle(fontSize: 12, fontWeight: FontWeight.w400);
