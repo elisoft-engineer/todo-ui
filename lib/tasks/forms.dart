54cd2710bf2029f203dd415f44f05db504acd6f0
Elkana Maina <elisoft.engineer@gmail.com>
2025-06-06 18:41:37 +0300

    Add TaskEditForm

--- lib/tasks/forms.dart
+++ lib/tasks/forms.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo/components/notifications.dart';
 import 'package:todo/core/styles.dart';
+import 'package:todo/tasks/models.dart';
 import 'package:todo/tasks/services.dart';
 
 class TaskCreationForm extends StatefulWidget {
@@ -73,32 +74,39 @@ class _TaskCreationFormState extends State<TaskCreationForm> {
               spacing: 24,
               children: [
                 Text('New Task', style: CustomTextStyles.h2),
-                TextFormField(
-                  controller: _detailController,
-                  keyboardType: TextInputType.text,
-                  style: CustomTextStyles.b2,
-                  maxLines: 3,
-                  decoration: InputDecoration(
-                    labelText: 'Detail',
-                    errorText: _formErrors?['title']?.join(', '),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: colorScheme.primary,
-                        width: 2,
+                SizedBox(
+                  width: MediaQuery.sizeOf(context).width,
+                  child: Expanded(
+                    child: TextFormField(
+                      controller: _detailController,
+                      keyboardType: TextInputType.text,
+                      style: CustomTextStyles.b2,
+                      maxLines: 3,
+                      decoration: InputDecoration(
+                        labelText: 'Detail',
+                        errorText: _formErrors?['detail']?.join(', '),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: colorScheme.primary,
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: colorScheme.inversePrimary,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
                       ),
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                    ),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: colorScheme.inversePrimary),
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Detail is required';
+                        }
+                        return null;
+                      },
                     ),
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Detail is required';
-                    }
-                    return null;
-                  },
                 ),
                 Slider.adaptive(
                   value: _priority,
@@ -146,6 +154,123 @@ class _TaskCreationFormState extends State<TaskCreationForm> {
           ),
         ),
       ),
+    );
+  }
+}
+
+class TaskEditForm extends StatefulWidget {
+  const TaskEditForm({super.key, required this.task});
+  final Task task;
+
+  @override
+  State<TaskEditForm> createState() => _TaskEditFormState();
+}
+
+class _TaskEditFormState extends State<TaskEditForm> {
+  final _formKey = GlobalKey<FormState>();
+  late TextEditingController _detailController = TextEditingController(
+    text: widget.task.detail,
+  );
+  late double _priority = widget.task.priority.toDouble();
+  Map<String, dynamic>? _formErrors;
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    return AlertDialog(
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text('Edit task', style: CustomTextStyles.h2),
+          IconButton(
+            onPressed: () => Navigator.of(context).pop(),
+            icon: Icon(Icons.close),
+          ),
+        ],
+      ),
+      content: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          spacing: 20,
+          children: [
+            SizedBox(
+              width: MediaQuery.sizeOf(context).width,
+              child: Expanded(
+                child: TextFormField(
+                  controller: _detailController,
+                  keyboardType: TextInputType.text,
+                  style: CustomTextStyles.b2,
+                  maxLines: 3,
+                  decoration: InputDecoration(
+                    labelText: 'Detail',
+                    errorText: _formErrors?['detail']?.join(', '),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: colorScheme.primary,
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: colorScheme.inversePrimary),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Detail is required';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+            ),
+            Slider.adaptive(
+              value: _priority,
+              label: 'Priority',
+              max: 5,
+              min: 1,
+              divisions: 4,
+              onChanged: (double value) {
+                setState(() {
+                  _priority = value;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        Expanded(
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.edit, color: colorScheme.primary),
+                style: IconButton.styleFrom(
+                  backgroundColor: colorScheme.inversePrimary,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.delete, color: colorScheme.error),
+                style: IconButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(87, 235, 71, 42),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.keyboard_double_arrow_right_outlined),
+                style: IconButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(75, 94, 110, 109),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
