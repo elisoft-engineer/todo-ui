de5d2c42ab7ea51f5a00efd34190aeb536f07c9d
Elkana Maina <elisoft.engineer@gmail.com>
2025-06-09 10:26:08 +0300

    Make theme mode dynamic

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:todo/auth/signin_page.dart';
+import 'package:todo/core/notifiers.dart';
 import 'package:todo/tasks/main.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
@@ -16,22 +17,27 @@ class Main extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Todo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.teal,
-          brightness: Brightness.light,
-        ),
-        fontFamily: "Urbanist",
-      ),
-      routes: {
-        'signin': (context) => SigninPage(title: "Sign In"),
-        'tasks': (context) => TaskViewSet(),
+    return ValueListenableBuilder(
+      valueListenable: themeModeNotifier,
+      builder: (context, themeMode, child) {
+        return MaterialApp(
+          title: 'Todo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.teal,
+              brightness: themeMode,
+            ),
+            fontFamily: "Urbanist",
+          ),
+          routes: {
+            'signin': (context) => SigninPage(title: "Sign In"),
+            'tasks': (context) => TaskViewSet(),
+          },
+          initialRoute: 'signin',
+          navigatorKey: navigatorKey,
+          debugShowCheckedModeBanner: false,
+        );
       },
-      initialRoute: 'signin',
-      navigatorKey: navigatorKey,
-      debugShowCheckedModeBanner: false,
     );
   }
 }
