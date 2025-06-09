ef3b98d9a17443713ee9dc72917f6e5fab02de50
Elkana Maina <elisoft.engineer@gmail.com>
2025-06-09 12:25:45 +0300

    Add tasksFutureNotifier

--- lib/core/notifiers.dart
+++ lib/core/notifiers.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:todo/tasks/models.dart';
 
-ValueNotifier<Brightness> themeModeNotifier = ValueNotifier(Brightness.light);
+final ValueNotifier<Brightness> themeModeNotifier = ValueNotifier(
+  Brightness.light,
+);
+final ValueNotifier<Future<List<Task>>> tasksFutureNotifier =
+    ValueNotifier<Future<List<Task>>>(Future.value([]));
